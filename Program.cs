1f73801aa95fc26e0ed3a0a76294266f93fa9d4e
Ezequiel de Jesus Urbina Zeledon <ezequiel.urbina24u@std.uni.edu.ni>
2024-04-19 16:54:25 -0600

    Reparación de errores graves en el core del front.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using wsmcbl.front.Data;
 using wsmcbl.front.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
 builder.Services.AddHttpClient();
-
 builder.Services.AddSingleton<StudentController>();   
-builder.Services.AddSingleton<TariffController>();   
-
+builder.Services.AddSingleton<TariffController>();  
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
--- wwwroot/js/sb-admin-2.js
+++ wwwroot/js/sb-admin-2.js
@@ -2,30 +2,30 @@
   "use strict"; // Start of use strict
 
   // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+  $(document).on('click', '#sidebarToggle, #sidebarToggleTop', function(e) {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
   // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
+  $(window).on('resize', function() {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
     
     // Toggle the side navigation when window is resized below 480px
     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
       $("body").addClass("sidebar-toggled");
       $(".sidebar").addClass("toggled");
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  $(document).on('mousewheel DOMMouseScroll wheel', 'body.fixed-nav .sidebar', function(e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
         delta = e0.wheelDelta || -e0.detail;
@@ -53,4 +53,12 @@
     e.preventDefault();
   });
 
+  // Close menu when clicking on a menu item
+$(document).on('click', '.sidebar .collapse-item', function() {
+  // Close the collapsed menu
+  $('.sidebar .collapse').collapse('hide');
+});
+
+
 })(jQuery); // End of use strict
+
