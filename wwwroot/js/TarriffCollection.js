321d9d28267dadac4fdbf2b561209e3d823d07a7
Ezequiel de Jesus Urbina Zeledon <ezequiel.urbina24u@std.uni.edu.ni>
2024-04-21 20:21:42 -0600

    Add TarriffCollection file css and js.

---
+++ wwwroot/js/TarriffCollection.js
@@ -0,0 +1,11 @@
+
+    document.getElementById("amoun").addEventListener("input", (event)=>{
+
+        var montoTotal = document.getElementById("modalAmount").innerText;
+
+        var pagaCon = event.target.value;
+
+        var cambio = document.getElementById("chamge");
+        cambio.value = (pagaCon - montoTotal);
+    })
+
