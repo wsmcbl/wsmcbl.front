6f12903763c17d16de5001ae302c8079c1630674
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-08-09 21:50:44 -0600

    Feat: Add ModalView for PDF document in EnrollStudentView.razor

--- src/View/Secretary/EnrollmentStudent/Dto/StudentFullDto.cs
+++ src/View/Secretary/EnrollmentStudent/Dto/StudentFullDto.cs
@@ -5,120 +5,50 @@ namespace wsmcbl.src.View.Secretary.EnrollmentStudent.Dto;
 
 public class StudentFullDto
 {
-    [JsonProperty("studentId")]
-    public string StudentId { get; set; }
-    
-    [JsonProperty("name")]
-    public string Name { get; set; }
-    
-    [JsonProperty("secondName")]
-    public string SecondName { get; set; }
-    
-    [JsonProperty("surname")]
-    public string Surname { get; set; }
-    
-    [JsonProperty("secondSurname")]
-    public string SecondSurname { get; set; }
-    
-    [JsonProperty("isActive")]
-    public bool IsActive { get; set; }
-    
-    [JsonProperty("sex")]
-    public bool Sex { get; set; }
-    
-    [JsonProperty("birthday")]
+    public string studentId { get; set; }
+    public string name { get; set; }
+    public string secondName { get; set; }
+    public string surname { get; set; }
+    public string secondSurname { get; set; }
+    public bool sex { get; set; }
     public Date birthday { get; set; }
+    public string religion { get; set; }
+    public string diseases { get; set; }
     
-    [JsonProperty("religion")]
-    public string Religion { get; set; }
-    
-    [JsonProperty("diseases")]
-    public string Diseases { get; set; }
-    
-    [JsonProperty("parents")]
-    public List<Parent> Parents { get; set; }
-    
-    [JsonProperty("tutor")]
-    public Tutor Tutor { get; set; }
-    
-    [JsonProperty("measurements")]
-    public Measurements Measurements { get; set; }
-    
-    [JsonProperty("studentId")]
-    public File File { get; set; }
+    public string address { get; set; }
+    public bool isActive { get; set; }
+    public File file { get; set; }
+    public Tutor tutor { get; set; }
+    public List<Parent> parents { get; set; }
+    public Measurements measurements { get; set; }
 }
-
-
-
-
 public class Parent
 {
-    [JsonProperty("parentId")]
     public string parentId { get; set; }
-    
-    [JsonProperty("sex")]
-    public bool Sex { get; set; }
-    
-    [JsonProperty("name")]
-    public string Name { get; set; }
-    
-    [JsonProperty("address")]
-    public string Address { get; set; }
-    
-    [JsonProperty("idCard")]
-    public string IdCard { get; set; }
-    
-    [JsonProperty("phone")]
-    public string Phone { get; set; }
-    
-    [JsonProperty("occupation")]
-    public string Occupation { get; set; }
+    public bool sex { get; set; }
+    public string name { get; set; }
+    public string idCard { get; set; }
+    public string occupation { get; set; }
 }
-
 public class Tutor
-{
-    [JsonProperty("tutorId")]
-    public string TutorId { get; set; }
-    
-    [JsonProperty("name")]
-    public string Name { get; set; }
-    
-    [JsonProperty("phone")]
-    public string Phone { get; set; }
+{ 
+    public string tutorId { get; set; }
+    public string name { get; set; }
+    public string phone { get; set; }
 }
-
 public class Measurements
-{
-    [JsonProperty("measurementId")]
-    public string MeasurementId { get; set; }
-    
-    [JsonProperty("height")]
-    public int Height { get; set; }
-    
-    [JsonProperty("weight")]
-    public int Weight { get; set; }
+{ 
+    public int measurementId { get; set; }
+    public int height { get; set; }
+    public int weight { get; set; }
 }
-
 public class File
 {
-    [JsonProperty("fileId")]
-    public string FileId { get; set; }
-    
-    [JsonProperty("transferSheet")]
-    public bool TransferSheet { get; set; }
-    
-    [JsonProperty("birthDocument")]
-    public bool BirthDocument { get; set; }
-    
-    [JsonProperty("parentIdentifier")]
-    public bool ParentIdentifier { get; set; }
-    
-    [JsonProperty("updatedGradeReport")]
-    public bool UpdatedGradeReport { get; set; }
-    
-    [JsonProperty("conductDocument")]
-    public bool ConductDocument { get; set; }
-    
-    [JsonProperty("financialSolvency")]
-    public bool FinancialSolvency { get; set; }
+    public int fileId { get; set; }
+    public bool transferSheet { get; set; }
+    public bool birthDocument { get; set; }
+    public bool parentIdentifier { get; set; }
+    public bool updatedGradeReport { get; set; }
+    public bool conductDocument { get; set; }
+    public bool financialSolvency { get; set; }
 }
