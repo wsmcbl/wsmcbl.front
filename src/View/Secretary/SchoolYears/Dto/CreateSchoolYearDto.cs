43d41f9a1362326299d80d61e83912b02af6da0e
Ezequiel Urbina <Ezequielurbina100@gmail.com>
2024-08-17 09:19:48 -0600

    Push

--- src/View/Secretary/SchoolYears/Dto/CreateSchoolYearDto.cs
+++ src/View/Secretary/SchoolYears/Dto/CreateSchoolYearDto.cs
@@ -3,5 +3,5 @@ namespace wsmcbl.src.View.Secretary.SchoolYears.Dto;
 public class CreateSchoolYearDto
 {
     public List<GradeDto>? degrees { get; set; }
-    public List<SchoolYearTariffs>? tariffs { get; set; }
+    public List<SchoolYearTariffs>? tariffs { get; set; } 
 }
