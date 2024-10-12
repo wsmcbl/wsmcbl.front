561d36ee831fe32a9a484262dfb0e695aa505af1
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-10-12 14:33:07 -0600

    Change Permison ON deploy

--- init-nextcloud.sh
+++ init-nextcloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Instalar netcat-openbsd
+# Instalar netcat-openbsd como root
 apt-get update && apt-get install -y netcat-openbsd
 
 # Esperar a que la base de datos esté disponible
@@ -9,9 +9,9 @@ until nc -z db_nextcloud 3306; do
   sleep 5
 done
 
-# Ejecutar comandos php occ directamente, ya que se ejecutarán como www-data
-php occ config:system:set default_language --value=es
-php occ config:system:set theme --value=light
-php occ config:system:set maintenance_window_start --value=1
-php occ config:system:set overwriteprotocol --value=https
-php occ config:system:set overwrite.cli.url --value=https://nextcloud.cbl-edu.com
+# Ejecutar comandos php occ como www-data
+su - www-data -s /bin/sh -c "php occ config:system:set default_language --value=es"
+su - www-data -s /bin/sh -c "php occ config:system:set theme --value=light"
+su - www-data -s /bin/sh -c "php occ config:system:set maintenance_window_start --value=1"
+su - www-data -s /bin/sh -c "php occ config:system:set overwriteprotocol --value=https"
+su - www-data -s /bin/sh -c "php occ config:system:set overwrite.cli.url --value=https://nextcloud.cbl-edu.com"
