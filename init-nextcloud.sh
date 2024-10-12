b93c75f8a2f9000f6f3b10d64880cca5c5663874
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-10-12 14:25:09 -0600

    Refact INIT-Nextcloud V4

--- init-nextcloud.sh
+++ init-nextcloud.sh
@@ -9,13 +9,9 @@ until nc -z db_nextcloud 3306; do
   sleep 5
 done
 
-# Cambiar el idioma por defecto a español
-php occ config:system:set default_language --value=es
-# Establecer el tema por defecto
-php occ config:system:set theme --value=light
-# Establecer la ventana de mantenimiento
-php occ config:system:set maintenance_window_start --value=1
-# Configurar el protocolo de sobreescritura.
-php occ config:system:set overwriteprotocol --value=https
-# Configurar la URL de sobreescritura
-php occ config:system:set overwrite.cli.url --value=https://nextcloud.cbl-edu.com
+# Ejecutar comandos php occ como el usuario correcto
+sudo -u "#33" php occ config:system:set default_language --value=es
+sudo -u "#33" php occ config:system:set theme --value=light
+sudo -u "#33" php occ config:system:set maintenance_window_start --value=1
+sudo -u "#33" php occ config:system:set overwriteprotocol --value=https
+sudo -u "#33" php occ config:system:set overwrite.cli.url --value=https://nextcloud.cbl-edu.com
