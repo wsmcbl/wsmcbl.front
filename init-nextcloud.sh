f96950497fc271f37e9b7cb55bd41b7c34ac1b24
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-10-12 14:29:00 -0600

    FIX docker-compose.yml and init-nextcloud.sh

--- init-nextcloud.sh
+++ init-nextcloud.sh
@@ -9,9 +9,9 @@ until nc -z db_nextcloud 3306; do
   sleep 5
 done
 
-# Ejecutar comandos php occ como el usuario correcto
-sudo -u "#33" php occ config:system:set default_language --value=es
-sudo -u "#33" php occ config:system:set theme --value=light
-sudo -u "#33" php occ config:system:set maintenance_window_start --value=1
-sudo -u "#33" php occ config:system:set overwriteprotocol --value=https
-sudo -u "#33" php occ config:system:set overwrite.cli.url --value=https://nextcloud.cbl-edu.com
+# Ejecutar comandos php occ directamente, ya que se ejecutarán como www-data
+php occ config:system:set default_language --value=es
+php occ config:system:set theme --value=light
+php occ config:system:set maintenance_window_start --value=1
+php occ config:system:set overwriteprotocol --value=https
+php occ config:system:set overwrite.cli.url --value=https://nextcloud.cbl-edu.com
