fe51729c9671e75b38bab296ea7662efd60c6785
Ezequiel Urbina <Ezequielurbina100@gmail.com>
2024-11-16 13:29:09 -0600

    Update: TopNavBar.razor.cs

--- src/System/View/Base/TopNavBar.razor.cs
+++ src/System/View/Base/TopNavBar.razor.cs
@@ -7,13 +7,7 @@ public class TopNavBar_razor : ComponentBase
 {
     [Inject] private CustomAuthenticationStateProvider AuthStateProvider { get; set; }
     [Inject] private Navigator Navigator { get; set; }
-
-
-    protected override Task OnParametersSetAsync()
-    {
-        return base.OnParametersSetAsync();
-    }
-
+    
     public async Task LogOut()
     {
         await AuthStateProvider.MarkUserAsLoggedOut();
