2213a4e3ecd3b7e53234f1fb41ae8e012fa34fbb
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-11-18 02:33:01 -0600

    Refactor: update UserDto class

--- src/System/View/Base/UserDto.cs
+++ src/System/View/Base/UserDto.cs
@@ -3,14 +3,21 @@ namespace wsmcbl.src.View.Base;
 public class UserDto
 {
     public string name { get; set; } = null!;
-    public string secondName { get; set; } = null!;
+    public string secondName { get; set; }
     public string surname { get; set; } = null!;
-    public string secondSurname { get; set; } = null!;
+    public string secondSurname { get; set; }
     public string email { get; set; } = null!;
     public bool isActive { get; set; }
     public string role { get; set; } = null!;
 
     public UserDto()
     {
+        name = "Default";
+        surname = "User";
+    }
+
+    public string getName()
+    {
+        return $"{name} {surname}";
     }
 }
