14bca164d476c83d3cba729b4b64693e0f221816
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-11-20 20:24:35 -0600

    Add: - RevenueReport.razor

---
+++ src/System/View/Accounting/Reports/RevenueReport.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
+namespace wsmcbl.src.View.Accounting.Reports;
+
+public partial class RevenueReport : ComponentBase
+{
+    private bool IsLoad()
+    {
+        return false;
+    }
+}
