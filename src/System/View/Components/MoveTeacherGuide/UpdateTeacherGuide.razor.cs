9be8e104f095d862f815b487ed29173bc3b7645d
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-12-23 11:16:48 -0600

    Rename Files

--- src/System/View/Components/MoveTeacherGuide/UpdateTeacherGuide.razor.cs
+++ src/System/View/Components/MoveTeacherGuide/UpdateTeacherGuide.razor.cs
@@ -8,13 +8,12 @@ public partial class UpdateTeacherGuide : ComponentBase
 {
     
     [Parameter] public string? TeacherNow { get; set; } = "N/A";
-    [Parameter] public string? Enrollment { get; set; } = "N/A";
+    [Parameter] public string? EnrollmentNow { get; set; } = "N/A";
     [Inject] MoveTeacherGuideFromEnrollmentController Controller { get; set; } = null!;
     [Inject] Notificator Notificator { get; set; } = null!;
     private List<TeacherNoGuideDto> TeacherAvailable{ get; set; } = [];
-    private ChangeTeacherDto Teacher { get; set; } = new ChangeTeacherDto();
-
-    private string NewTeacher { get; set; } = string.Empty;
+    private ChangeTeacherDto NewTeacher { get; set; } = new ChangeTeacherDto();
+    private string NewTeacherId { get; set; } = string.Empty;
 
     
     protected override async Task OnParametersSetAsync()
@@ -24,16 +23,15 @@ public partial class UpdateTeacherGuide : ComponentBase
 
     private async Task UpdateTeacherG()
     {
-        Teacher.newTeacherId = NewTeacher;
-        Teacher.enrollmentId = Enrollment; 
-        var response = await Controller.UpdateTeacherGuide(Teacher);
+        NewTeacher.newTeacherId = NewTeacherId;
+        NewTeacher.enrollmentId = EnrollmentNow; 
+        var response = await Controller.UpdateTeacherGuide(NewTeacher);
 
         if (response)
         {
             await Notificator.ShowSuccess("Exito", "Hemos actualizado con exito al maestro guia");
             return;
         }
-        
         await Notificator.ShowError("Error", "No pudimos actualizar al maestro guia.");
     }
 }
