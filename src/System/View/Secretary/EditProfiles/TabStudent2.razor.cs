27ae9cf8e672eb6716334a9bce0a32f9903e2eaf
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-12-24 09:23:23 -0600

    Fix } in TariffCollectionView.razor and fix TabStudent2.razor

---
+++ src/System/View/Secretary/EditProfiles/TabStudent2.razor.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components;
+using wsmcbl.src.Model.Secretary;
+using wsmcbl.src.View.Secretary.SchoolYears;
+
+namespace wsmcbl.src.View.Secretary.EditProfiles;
+
+public partial class TabStudent2 : ComponentBase
+{
+    [Parameter] public StudentEntity? Student { get; set; }
+    [Parameter] public bool SelectRepeat { get; set; }
+    [Parameter] public EventCallback<bool> SelectRepeatChanged { get; set; }
+    [Parameter] public int Age { get; set; }
+    [Parameter] public string? Sex { get; set; }
+    
+    [Parameter] public bool IsRepeating { get; set; }
+    
+    protected override Task OnParametersSetAsync()
+    {
+        if (Student != null)
+        {
+            Age = Student.birthday.AgeCompute();
+            Sex = Student.sex ? "true" : "false";
+        }
+        
+        if (Student?.parents != null && Student.parents.Count >= 2)
+        {
+            Student.parents[0].sex = false;
+            Student.parents[1].sex = true;
+        }
+
+        return Task.CompletedTask;
+    }
+    
+}
