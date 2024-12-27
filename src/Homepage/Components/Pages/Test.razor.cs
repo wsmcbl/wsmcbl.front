5de3c0e3ba59cff0ea3a3111aa475d30495d4347
Ezequiel Urbina <ezequielurbina100@gmail.com>
2024-12-27 11:50:58 -0600

    Refact order of file in Home Project. Create components

---
+++ src/Homepage/Components/Pages/Test.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
+namespace wsmcbl.src.Components.Pages;
+
+public  class Test_ : ComponentBase
+{
+    protected  void Escribir()
+    {
+        Console.WriteLine("Hola wapo");
+    } 
+}
