0e3d58ededadd4922fb6210496166de7460dc746
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-18 11:09:38 -0600

    Refactor: EnrollStudentView files

--- src/System/View/Secretary/EnrollStudent/EnrollStudentView.razor.cs
+++ src/System/View/Secretary/EnrollStudent/EnrollStudentView.razor.cs
@@ -1,66 +1,61 @@
 using Microsoft.AspNetCore.Components;
 using wsmcbl.src.Controller;
 using wsmcbl.src.Utilities;
+using wsmcbl.src.View.Base;
 using wsmcbl.src.View.Secretary.EnrollStudent.Dto;
 using StudentEntity = wsmcbl.src.Model.Secretary.StudentEntity;
 
 namespace wsmcbl.src.View.Secretary.EnrollStudent;
 
-public partial class EnrollStudentView : ComponentBase
+public partial class EnrollStudentView : BaseView
 {
     [Parameter] public string StudentId { get; set; } = null!;
-    [Inject] protected EnrollStudentController Controller { get; set; } = null!;
-    [Inject] protected Notificator Notificator { get; set; } = null!;
+
     [Inject] protected Navigator Navigator { get; set; } = null!;
+    [Inject] protected Notificator Notificator { get; set; } = null!;
+    [Inject] protected EnrollStudentController Controller { get; set; } = null!;
     
-     
-    private StudentEntity? Student { get; set; }
     private int DiscountId { get; set; }
-    private List<DegreeBasicDto>? Degrees { get; set; }
-    
+    private StudentEntity? Student { get; set; }
+    private List<DegreeBasicDto>? DegreeList { get; set; }
+
     private int Age { get; set; }
-    private string? EnrollmentIdSelected { get; set; }
-    private bool IsStudentsEnrollment { get; set; }
     private bool IsRepeating { get; set; }
-    private bool isLoading = true;
+    private bool IsStudentsEnrollment { get; set; }
+    private string? EnrollmentIdSelected { get; set; }
+
     
     protected override async Task OnParametersSetAsync()
     {
-        
-        if (isLoading)
-        {
-            EnrollSheetPdf = [];
-            await LoadStudentInformation();
-            isLoading = false;
-        }
-        
+        EnrollSheetPdf = [];
+        await LoadStudentInformation();
     }
-    
+
     private async Task LoadStudentInformation()
-    {  
+    {
         var result = await Controller.GetInfoStudent(StudentId);
-        Degrees = await Controller.GetDegreeBasicList();
-        
+        DegreeList = await Controller.GetDegreeBasicList();
+
         IsStudentsEnrollment = result.enrollmentId != null;
         Student = result.student;
         DiscountId = result.discountId;
-        
-        if (!Student.parents!.Any())
+
+        if (Student.parents!.Count == 0)
         {
             Student.parents = [];
         }
     }
-    
+
     private void OnRepeatSelectionChanged(bool value)
     {
         IsRepeating = value;
     }
-    
+
     private async Task SaveEnrollment()
     {
         if (Student != null && !Student.IsStudentValid())
         {
-           await Notificator.ShowInformation("Los campos marcados con (*) son obligatorios.");
+            await Notificator.ShowInformation("Los campos marcados con (*) son obligatorios.");
             return;
         }
 
@@ -69,7 +64,7 @@ public partial class EnrollStudentView : ComponentBase
             await Notificator.ShowInformation("Debe ingresar los datos del tutor, o bien dejar en blanco.");
             return;
         }
-        
+
         if (!Student!.IsTutorValid())
         {
             await Notificator.ShowInformation("Por favor, ingrese todos los campos del tutor.");
@@ -88,7 +83,7 @@ public partial class EnrollStudentView : ComponentBase
                 "Por favor seleccione un grado y una sección para el estudiante.");
             return;
         }
-        
+
         var response = await Controller.SaveEnrollment(Student, EnrollmentIdSelected, DiscountId, IsRepeating);
         if (response)
         {
@@ -104,14 +99,14 @@ public partial class EnrollStudentView : ComponentBase
         {
             return true;
         }
-        
+
         for (var index = Student.parents.Count - 1; index >= 0; index--)
         {
             if (Student.parents[index].isTutorPartiallyFilled())
             {
                 return false;
             }
-             
+
             if (Student.parents[index].isTutorEmpty())
             {
                 Student.parents[index].init();
@@ -122,15 +117,20 @@ public partial class EnrollStudentView : ComponentBase
     }
 
     private byte[]? EnrollSheetPdf { get; set; }
+
     private async Task PrintEnrollSheet(string studentId)
     {
         EnrollSheetPdf = await Controller.GetEnrollSheetPdf(studentId);
-
         if (EnrollSheetPdf.Length == 0)
         {
             return;
         }
 
         await Navigator.ShowPdfModal();
+    }
+
+    protected override bool IsLoading()
+    {
+        return Student == null || DegreeList == null || DegreeList.Count == 0;
     }
 }
