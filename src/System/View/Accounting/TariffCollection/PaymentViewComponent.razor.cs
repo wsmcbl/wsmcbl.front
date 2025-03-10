f8aeded71436b9b4a2d822fa868853e5f9d88757
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-10 10:13:16 -0600

    Feat: add ExonerateArrearsChecked property in PaymentViewComponent class

--- src/System/View/Accounting/TariffCollection/PaymentViewComponent.razor.cs
+++ src/System/View/Accounting/TariffCollection/PaymentViewComponent.razor.cs
@@ -9,18 +9,29 @@ public partial class PaymentViewComponent : ComponentBase
 {
     [Inject] protected Navigator Navigator { get; private set; } = null!;
     [Parameter] public List<TariffEntity> TariffList { get; set; } = null!;
-    
-    protected double Arrears { get; set; }
-    protected double Subtotal { get; set; }
-    protected double Discount { get; set; }
-    protected double Total { get; set; }
 
-    protected double AmountToDivide { get; set; }
-    private bool AreArrearsApply { get; set; } = true;
+    private double Arrears { get; set; }
+    private double Subtotal { get; set; }
+    private double Discount { get; set; }
+    private double Total { get; set; }
+
+    private double AmountToDivide { get; set; }
+
+    private bool ExonerateArrears;
+    private bool ExonerateArrearsChecked
+    {
+        get => ExonerateArrears;
+        set
+        {
+            ExonerateArrears = value;
+            Total += (ExonerateArrears ? -1 : 1) * Arrears;
+            Total = Math.Round(Total, 2);
+        }
+    }
 
 
     protected override void OnParametersSet()
-    {        
+    {
         ComputeTotal();
         AmountToDivide = 0;
     }
@@ -39,26 +50,19 @@ public partial class PaymentViewComponent : ComponentBase
             Arrears += item.Arrears;
             Total += item.Total;
         }
-        
+
         Total = Math.Round(Total, 2);
     }
-    
-    [Parameter] public EventCallback<List<DetailDto>> OnComplexObjectCreated { get; set; }
+
+    [Parameter] public EventCallback<List<DetailDto>> CollectTariff { get; set; }
 
     private async Task CreateDetail(double amountToPay)
     {
-        var detail = TariffList.MapToDto(AreArrearsApply, amountToPay);
-        await OnComplexObjectCreated.InvokeAsync(detail);
+        var detail = TariffList.MapToDto(!ExonerateArrears, amountToPay);
+
+        await CollectTariff.InvokeAsync(detail);
         TariffList = [];
         await Navigator.HideModal("PaymentView");
-    }
-    
-    protected void ExonerateArrears(ChangeEventArgs e)
-    {
-        AreArrearsApply = (bool)e.Value!;
-
-        Total += (AreArrearsApply ? -1 : 1) * Arrears;
-        Total = Math.Round(Total, 2);
     }
 
     private void HandleInputChange(ChangeEventArgs e)
@@ -66,15 +70,12 @@ public partial class PaymentViewComponent : ComponentBase
         AmountToDivide = string.IsNullOrEmpty(e.Value?.ToString()) ? 0 : Convert.ToDouble(e.Value);
         StateHasChanged();
     }
-    
-    private async Task HandleKeyDown(KeyboardEventArgs e)
+
+    private async Task HandleKeyDown(KeyboardEventArgs key)
     {
-        if (AmountToDivide > 0)
+        if (AmountToDivide > 0 && key is { Key: "Enter", CtrlKey: true })
         {
-            if (e.Key == "Enter" && e.CtrlKey)
-            {
-                await CreateDetail(AmountToDivide);
-            }
+            await CreateDetail(AmountToDivide);
         }
     }
 }
