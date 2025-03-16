417fa174c242316ac60288726f81e59561eb2eb5
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 16:32:22 -0600

    Update: SubjectDto class

--- src/System/View/Secretary/Schoolyear/SubjectDto.cs
+++ src/System/View/Secretary/Schoolyear/SubjectDto.cs
@@ -2,5 +2,10 @@ namespace wsmcbl.src.View.Secretary.Schoolyear;
 
 public class SubjectDto
 {
-    
+    public int areaId { get; set; }
+    public string name { get; set; } = null!;
+    public bool isMandatory { get; set; }
+    public int semester { get; set; }
+    public string initials { get; set; } = null!;
+    public int number { get; set; }
 }
