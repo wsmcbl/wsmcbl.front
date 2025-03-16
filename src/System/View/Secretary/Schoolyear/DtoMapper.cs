f3931ce88b785db5fb2bb3197685d529c1229853
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 17:02:37 -0600

    Update: DtoMapper class

--- src/System/View/Secretary/Schoolyear/DtoMapper.cs
+++ src/System/View/Secretary/Schoolyear/DtoMapper.cs
@@ -1,36 +1,7 @@
-using wsmcbl.src.dto.Output;
-using wsmcbl.src.View.Components.Dto;
-using wsmcbl.src.View.Secretary.SchoolYears.Dto;
-using wsmcbl.src.View.Secretary.SchoolYears.Dto.CreateNewSchoolYear;
-using TariffCreateNewSchoolYearDto = wsmcbl.src.View.Secretary.SchoolYears.Dto.CreateNewSchoolYear.TariffCreateNewSchoolYearDto;
-
 namespace wsmcbl.src.View.Secretary.Schoolyear;
 
 public static class DtoMapper
 {
-    public static List<GradeCreateNewSchoolYearDto> ToListDto(this List<DegreeDto>? listGrade)
-    {
-        return listGrade == null ? [] : 
-            listGrade.Select(e => new GradeCreateNewSchoolYearDto(e)).ToList();
-    }
-    
-    public static List<TariffCreateNewSchoolYearDto> ToListDto(this List<SchoolyearTariffDto> tariffList)
-    {
-        return tariffList.Select(e => new TariffCreateNewSchoolYearDto(e)).ToList();
-    }
-    
-    public static TariffDataDto MapToTariffDataDto(SchoolyearTariffDto schoolyearTariffDto)
-    {
-        return new TariffDataDto
-        {
-            concept = schoolyearTariffDto.concept,
-            amount = schoolyearTariffDto.amount,
-            DueDateEntity = schoolyearTariffDto.OnlyDate.MapToDto(),
-            typeId = schoolyearTariffDto.type,
-            modality = schoolyearTariffDto.modality
-        };
-    }
-    
     public static int AgeCompute(this DateOnly? birthday)
     {
         if (birthday == null)
