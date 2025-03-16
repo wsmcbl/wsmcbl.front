dc887507f9903291b803e02f0bff9f9353cf1770
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 15:05:59 -0600

    Update: rename SchoolyearListView files

--- src/System/View/Secretary/Schoolyear/SchoolyearListView.razor.cs
+++ src/System/View/Secretary/Schoolyear/SchoolyearListView.razor.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Components;
 using wsmcbl.src.Controller;
 using wsmcbl.src.View.Secretary.SchoolYears.Dto;
 
-namespace wsmcbl.src.View.Secretary.SchoolYears;
+namespace wsmcbl.src.View.Secretary.Schoolyear;
 
-public class ListSchoolYears : ComponentBase
+public class SchoolyearList : ComponentBase
 {
     protected List<SchoolYearDto>? SchoolYear;
     [Inject] protected CreateSchoolyearController Controller { get; set; } = null!;
