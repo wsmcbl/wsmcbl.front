df48793d8782b04992da00129e691a592268f95d
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 16:55:01 -0600

    Update: TariffDataDto class

--- src/System/View/Secretary/Schoolyear/TariffData/TariffDataDto.cs
+++ src/System/View/Secretary/Schoolyear/TariffData/TariffDataDto.cs
@@ -1,6 +1,14 @@
+using wsmcbl.src.View.Components.Dto;
+
 namespace wsmcbl.src.View.Secretary.Schoolyear.TariffData;
 
 public class TariffDataDto
 {
-    
+    public int tariffDataId { get; set; }
+    public string concept { get; set; } = null!;
+    public decimal amount { get; set; }
+    public DateOnlyDto? dueDate { get; set; }
+    public int typeId { get; set; }
+    public int educationalLevel { get; set; }
+    public bool isActive { get; set; }
 }
