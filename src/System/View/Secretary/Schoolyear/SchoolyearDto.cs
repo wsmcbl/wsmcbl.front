aaf1d3c2d866bdd02d2ae18669150ea8f9e98e5d
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 16:35:28 -0600

    Update: SchoolyearDto class

--- src/System/View/Secretary/Schoolyear/SchoolyearDto.cs
+++ src/System/View/Secretary/Schoolyear/SchoolyearDto.cs
@@ -1,6 +1,21 @@
+using wsmcbl.src.Model.Accounting;
+using wsmcbl.src.View.Components.Dto;
+
 namespace wsmcbl.src.View.Secretary.Schoolyear;
 
 public class SchoolyearDto
 {
+    public string schoolyearId { get; set; } = null!;
+    public string label { get; set; } = null!;
+    public DateOnlyDto startDate { get; set; } = null!;
+    public DateOnlyDto deadLine { get; set; } = null!;
+    public bool isActive { get; set; }
+
+    public ExchangeRateEntity? exchangeRate { get; set; }
+
+    public List<PartialDto>? partialList { get; set; }
     
+    public List<DegreeSubjectDto>? degreeList { get; set; }
+    
+    public List<TariffDto>? tariffList { get; set; }    
 }
