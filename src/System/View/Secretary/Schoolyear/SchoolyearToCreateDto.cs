5d164bb4f0d5ccfcc573f3c899e7a07d48e54f0a
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 19:17:35 -0600

    Feat: add constructor in SchoolyearToCreateDto class

--- src/System/View/Secretary/Schoolyear/SchoolyearToCreateDto.cs
+++ src/System/View/Secretary/Schoolyear/SchoolyearToCreateDto.cs
@@ -2,6 +2,12 @@ namespace wsmcbl.src.View.Secretary.Schoolyear;
 
 public class SchoolyearToCreateDto
 {
-    public List<PartialToCreateDto> partialList { get; set; } = null!;
-    public List<TariffDto> tariffList { get; set; } = null!;
+    public List<PartialToCreateDto> partialList { get; set; }
+    public List<TariffDto> tariffList { get; set; }
+
+    public SchoolyearToCreateDto(List<TariffDto> tariffList, List<PartialToCreateDto> partialList)
+    {
+        this.tariffList = tariffList;
+        this.partialList = partialList;
+    }
 }
