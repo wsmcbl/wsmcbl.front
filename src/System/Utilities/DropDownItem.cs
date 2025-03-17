b774f81cb2c26f54f38bb1d15dffc3de58032b54
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 18:38:34 -0600

    Update: DropDownItem class

--- src/System/Utilities/DropDownItem.cs
+++ src/System/Utilities/DropDownItem.cs
@@ -1,7 +1,19 @@
+using wsmcbl.src.Model.Accounting;
+
 namespace wsmcbl.src.Utilities;
 
 public class DropDownItem
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+
+    public DropDownItem()
+    {
+    }
+    
+    public DropDownItem(TariffTypeEntity value)
+    {
+        Id = value.typeId;
+        Name = value.description;
+    }
 }
