a2b2f8a7a54bd3573c8f371aef9bcec5ebac618d
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 19:40:15 -0600

    Update: CreateTariffDataModal files

--- src/System/View/Secretary/Schoolyear/TariffData/CreateTariffDataModal.razor.cs
+++ src/System/View/Secretary/Schoolyear/TariffData/CreateTariffDataModal.razor.cs
@@ -1,17 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using wsmcbl.src.Controller;
 using wsmcbl.src.Utilities;
-using wsmcbl.src.View.Secretary.SchoolYears.Dto;
 
 namespace wsmcbl.src.View.Secretary.Schoolyear.TariffData;
 
 public partial class CreateTariffDataModal : ComponentBase
 {
     [Parameter] public List<DropDownItem>? TariffTypeItemList { get; set; }
-    [Inject] protected CreateSchoolyearController controller { get; set; } = null!;
+    [Inject] protected CreateTariffDataController controller { get; set; } = null!;
     [Inject] protected Notificator? Notificator { get; set; }
     
-    private SchoolyearTariffDto SelectedSchoolyearTariff = new();
+    private TariffDataDto Tariff = new();
     private List<DropDownItem> modalityItemList =
     [
         new() { Id = 1, Name = "Preescolar" },
@@ -19,10 +18,10 @@ public partial class CreateTariffDataModal : ComponentBase
         new() { Id = 3, Name = "Secundaria" }
     ];
     
-    private async Task SaveNewTariff(SchoolyearTariffDto schoolyearTariff)
+    private async Task CreateTariffData(TariffDataDto tariff)
     {
-        var response = await controller!.CreateNewTariff(schoolyearTariff);
-        if (response)
+        var response = await controller.CreateTariffData(tariff);
+        if (response == null)
         {
             await Notificator!.ShowSuccess("Se ha registrado el arancel correctamente.");
         }
