6e1eacf432b88f722c69fb3573f463920c629ff4
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-16 20:33:20 -0600

    Feat: add TariffDataListView files

---
+++ src/System/View/Secretary/Schoolyear/TariffData/TariffDataListView.razor.cs
@@ -0,0 +1,11 @@
+using wsmcbl.src.View.Base;
+
+namespace wsmcbl.src.View.Secretary.Schoolyear.TariffData;
+
+public partial class TariffDataListView : BaseView
+{
+    protected override bool IsLoading()
+    {
+        return false;
+    }
+}
