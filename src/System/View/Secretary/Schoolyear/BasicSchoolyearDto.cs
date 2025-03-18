ec05d83fa4c59d04dea613406158550f003b2714
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-18 13:26:18 -0600

    Update: BasicSchoolyearDto class

--- src/System/View/Secretary/Schoolyear/BasicSchoolyearDto.cs
+++ src/System/View/Secretary/Schoolyear/BasicSchoolyearDto.cs
@@ -1,10 +1,12 @@
+using wsmcbl.src.View.Components.Dto;
+
 namespace wsmcbl.src.View.Secretary.Schoolyear;
 
 public class BasicSchoolyearDto
 { 
     public string schoolyearId { get; set; } = null!;
     public string label { get; set; } = null!;
-    public string startDate { get; set; } = null!;
-    public string deadLine { get; set; } = null!; 
+    public DateOnlyDto startDate { get; set; } = null!;
+    public DateOnlyDto deadLine { get; set; } = null!; 
     public bool isActive { get; set; }
 }
