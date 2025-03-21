42ac2836b1c5e08cc9c9625ce592c632a888ea0a
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-20 21:43:13 -0600

    Update: delete DebtDto class

--- src/System/View/Accounting/TariffCollection/DebDto.cs
+++
@@ -1,8 +0,0 @@
-namespace wsmcbl.src.View.Accounting.TariffCollection;
-
-public class DebDto(string studentId, int tariffId, string authToken)
-{
-    public string studentId { get; set; } = studentId;
-    public int tariffId { get; set; } = tariffId;
-    public string authorizationToken { get; set; } = authToken;
-}
