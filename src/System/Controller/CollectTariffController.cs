4801d77cc20725a9513d82f14ec46b74686a3a2d
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-20 22:02:33 -0600

    Update: CollectTariffController class

--- src/System/Controller/CollectTariffController.cs
+++ src/System/Controller/CollectTariffController.cs
@@ -5,22 +5,19 @@ using wsmcbl.src.View.Accounting.TariffCollection;
 
 namespace wsmcbl.src.Controller;
 
-public class CollectTariffController
+public class CollectTariffController : BaseController
 {
-    private readonly ApiConsumerWithNotificator _apiConsumer;
     private readonly JwtClaimsService _JwtClaimsService;
 
-    public CollectTariffController(ApiConsumerFactory apiConsumerFactory, JwtClaimsService jwtClaimsService)
+    public CollectTariffController(ApiConsumerFactory apiConsumerFactory, JwtClaimsService jwtClaimsService) : base(apiConsumerFactory, "")
     {
-        _apiConsumer = apiConsumerFactory.WithNotificator;
         _JwtClaimsService = jwtClaimsService;
     }
 
     public async Task<Paginator<StudentEntity>> GetStudentList(PagedRequest pagedRequest)
     {
         Paginator<StudentEntity> defaultResult = new ();
-        return await _apiConsumer.GetAsync(Modules.Accounting, 
-            $"students{pagedRequest}", 
+        return await apiFactory.WithNotificator.GetAsync(Modules.Accounting,$"students{pagedRequest}", 
             defaultResult);
     }
 
@@ -31,39 +28,39 @@ public class CollectTariffController
         StudentId = studentId;
         var resource = $"students/{studentId}";
         StudentEntity defaultResult = new();
-        return await _apiConsumer.GetAsync(Modules.Accounting, resource, defaultResult);
+        return await apiFactory.WithNotificator.GetAsync(Modules.Accounting, resource, defaultResult);
     }
 
     public async Task<List<TariffEntity>> GetTariffListByStudentId(string? studentId)
     {
         var resource = $"students/{studentId}/tariffs"; 
         
-        var result = await _apiConsumer.GetAsync(Modules.Accounting, resource, new List<TariffDto>());
+        var result = await apiFactory.WithNotificator.GetAsync(Modules.Accounting, resource, new List<TariffDto>());
         return result.Select(e => e.ToEntity()).ToList();
     }
 
     public async Task<string> SendPay()
     {
         var defaultResult = new TransactionEntity();
-        var result = await _apiConsumer.PostAsync(Modules.Accounting, "transactions", Transaction, defaultResult);
+        var result = await apiFactory.WithNotificator.PostAsync(Modules.Accounting, "transactions", Transaction, defaultResult);
         return result.transactionId!;
     }
 
     public async Task<byte[]> GetInvoice(string transactionId)
     {
         var resource = $"documents/invoices/{transactionId}";
-        return await _apiConsumer.GetPdfAsync(Modules.Accounting, resource);
+        return await apiFactory.WithNotificator.GetPdfAsync(Modules.Accounting, resource);
     }
 
     public async Task<bool> CancelTransaction(string transactionId)
     {
         var resource = $"transactions/{transactionId}";
-        return await _apiConsumer.PutAsync(Modules.Accounting, resource, transactionId);
+        return await apiFactory.WithNotificator.PutAsync(Modules.Accounting, resource, transactionId);
     }
     
     public async Task<bool> EditDiscount(EditDiscountDto editDiscountDto)
     {
-        return await _apiConsumer.PutAsync(Modules.Accounting, "students", editDiscountDto);
+        return await apiFactory.WithNotificator.PutAsync(Modules.Accounting, "students", editDiscountDto);
     }
 
 
