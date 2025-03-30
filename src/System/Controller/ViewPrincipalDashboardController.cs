f46627fbfc9f0e1a139f50a6bd23ab14f179a977
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-30 15:53:38 -0600

    Update: ViewPrincipalDashboardController class
    Refs: (#24)

--- src/System/Controller/ViewPrincipalDashboardController.cs
+++ src/System/Controller/ViewPrincipalDashboardController.cs
@@ -3,24 +3,25 @@ using wsmcbl.src.View.Components.Charts;
 
 namespace wsmcbl.src.Controller;
 
-public class ViewPrincipalDashboardController
+public class ViewPrincipalDashboardController : BaseController
 {
-    private readonly ApiConsumerWithNotificator _apiConsumer;
-    
-    public ViewPrincipalDashboardController(ApiConsumerFactory apiConsumerFactory)
+    public ViewPrincipalDashboardController(ApiConsumerFactory apiFactory) : base(apiFactory, string.Empty)
     {
-        _apiConsumer = apiConsumerFactory.WithNotificator;
     }
     
     public async Task<DistributionDto> GetCurrentDistribution()
     {
         DistributionDto defaultResult = new();
-        return await _apiConsumer.GetAsync(Modules.Management, "students/distributions", defaultResult);
+        
+        return await apiFactory
+            .WithNotificator.GetAsync(Modules.Management, "students/distributions", defaultResult);
     }
 
     public async Task<RevenuesDto> GetCurrentRevenues()
     {
         RevenuesDto defaultResult = new();
-        return await _apiConsumer.GetAsync(Modules.Management, "revenues", defaultResult);
+        
+        return await apiFactory
+            .WithNotificator.GetAsync(Modules.Management, "revenues", defaultResult);
     }
 }
