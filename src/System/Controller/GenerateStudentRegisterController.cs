0142ba4f699bb197f1eba36a433f4de2b528b463
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-30 16:30:29 -0600

    Update: GenerateStudentRegisterController class
    Refs: (#24)

--- src/System/Controller/GenerateStudentRegisterController.cs
+++ src/System/Controller/GenerateStudentRegisterController.cs
@@ -9,40 +9,41 @@ public class GenerateStudentRegisterController : BaseController
 {
     private readonly IJSRuntime _jsRuntime;
 
-    public GenerateStudentRegisterController(ApiConsumerFactory apiFactory, IJSRuntime jsRuntime): base(apiFactory, "students/registers")
+    public GenerateStudentRegisterController(ApiConsumerFactory apiFactory, IJSRuntime jsRuntime) : base(apiFactory,
+        "students/registers")
     {
         _jsRuntime = jsRuntime;
     }
 
     public async Task<Paginator<RegisterDto>> GetStudentRegisterPaged(PagedRequest pagedRequest)
     {
-        Paginator<RegisterDto> defaultResult = new ();
-        
+        Paginator<RegisterDto> defaultResult = new();
+
         return await apiFactory
-            .WithNotificator.GetAsync(Modules.Secretary,$"{path}{pagedRequest}", defaultResult);
+            .WithNotificator.GetAsync(Modules.Management, $"{path}{pagedRequest}", defaultResult);
     }
 
     public async Task GetCurrentStudentRegister()
     {
         var resource = $"{path}/current/export";
-        
-        var fileBytes = await apiFactory.WithNotificator.GetByteFileAsync(Modules.Secretary, resource);
+
+        var fileBytes = await apiFactory.WithNotificator.GetByteFileAsync(Modules.Management, resource);
         if (fileBytes.Length <= 0)
         {
             throw new InternalException("Error al descargar el archivo.");
         }
-        
+
         var base64 = Convert.ToBase64String(fileBytes);
         var url = $"data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{base64}";
-        
+
         await _jsRuntime.InvokeVoidAsync("downloadFile", GetFileName(), url);
     }
-    
+
     private static string GetFileName()
     {
         var timeZoneUTC6 = TimeZoneInfo.FindSystemTimeZoneById("Central America Standard Time");
         var value = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZoneUTC6);
-        
+
         return $"Padrón.{value:ddMMyy.HHmm}.xlsx";
     }
 }
