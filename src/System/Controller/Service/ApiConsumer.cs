db54efcbc66d7777dfbfc2d9a7d103229e33e192
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-30 16:05:12 -0600

    Update: delete GetBackupAsync method in ApiConsumer class
    Refs: (#24)

--- src/System/Controller/Service/ApiConsumer.cs
+++ src/System/Controller/Service/ApiConsumer.cs
@@ -34,14 +34,6 @@ public class ApiConsumer
         return new Uri($"{_server}/{moduleDir}/{resource.TrimStart('/')}");
     }
 
-    public async Task<byte[]> GetBackupAsync(Modules module, string resource)
-    {
-        await LoadToken();
-        var response = await _httpClient.GetAsync(BuildUri(module, resource));
-        return await GenericHttpResponse(() => response.Content.ReadAsByteArrayAsync()!, [], response);
-    }
-
-    
     public async Task<T> GetAsync<T>(Modules module, string resource, T defaultResult)
     {
         await LoadToken();
