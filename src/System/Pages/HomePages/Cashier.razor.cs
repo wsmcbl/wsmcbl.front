7c5861a4b7d9185e17ab3f25e3f0bea1caa0bd65
Ezequiel Urbina <ezequielurbina100@gmail.com>
2025-04-26 12:03:57 -0600

    Add: All chart in Index.razor

--- src/System/Pages/HomePages/Cashier.razor.cs
+++ src/System/Pages/HomePages/Cashier.razor.cs
@@ -56,8 +56,4 @@ public partial class Cashier : ComponentBase
         
         await Navigator.ShowPdfModal();
     }
-    
-    
-    
-    
 }
--- src/System/Pages/Index.razor.cs
+++ src/System/Pages/Index.razor.cs
@@ -1,9 +1,62 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using wsmcbl.src.Controller;
+using wsmcbl.src.Utilities;
 
 namespace wsmcbl.src.Pages;
 
-public class Index_razor : ComponentBase
+public partial class Index: ComponentBase
 {
+    [Inject] GenerateDebtorReportController Controller { get; set; } = default!;
+    [Inject] Notificator Notificator { get; set; } = default!;
+    [Inject] Navigator Navigator { get; set; } = default!;
+    private byte[]? Pdf { get; set; } = [];
+    
+    private List<(string Display, string Value)> months = null!;
+    private string selectedMonthValue = "N/A";
+    private string selectedMonthDisplay = "N/A";
+    
+    
+    protected override Task OnParametersSetAsync()
+    {
+        months = GenerateMonthList(); 
+        selectedMonthValue = months.First().Value;
+        selectedMonthDisplay = months.First().Display;
+        return Task.CompletedTask;
+    }
+    private static List<(string Display, string Value)> GenerateMonthList()
+    {
+        var months = new List<(string Display, string Value)>();
+        var currentDate = DateTime.Now;
+
+        for (int i = 0; i <= 17; i++)
+        {
+            var date = currentDate.AddMonths(-i);
+            string display = date.ToString("MMMM/yyyy", new System.Globalization.CultureInfo("es-ES"));
+            string value = date.ToString("MM-yyyy");
+            months.Add((display, value));
+        }
+
+        return months;
+    }
+    private void OnMonthSelected((string Display, string Value) month)
+    {
+        selectedMonthValue = month.Value;
+        selectedMonthDisplay = month.Display;
+        StateHasChanged();
+    }
+
+    private async Task GetPdfContent()
+    { 
+        Pdf = await Controller.GetDebtorReport();
+        if (Pdf.Length == 0)
+        {
+            await Notificator.ShowError("No se pudo generar el documento.");
+            return;
+        }
+        
+        await Navigator.ShowPdfModal();
+    }
+
 }
 
