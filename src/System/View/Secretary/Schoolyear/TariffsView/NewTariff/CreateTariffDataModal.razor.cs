14f1978334586a0a0f44bafa6ce48edd9bd63db9
Ezequiel Urbina <ezequielurbina100@gmail.com>
2025-05-06 16:49:57 -0600

    Update and validate CRUD in TariffsView.razor

--- src/System/View/Secretary/Schoolyear/TariffsView/NewTariff/CreateTariffDataModal.razor.cs
+++ src/System/View/Secretary/Schoolyear/TariffsView/NewTariff/CreateTariffDataModal.razor.cs
@@ -10,35 +10,43 @@ public partial class CreateTariffDataModal : ComponentBase
     [Inject] protected CreateTariffDataController controller { get; set; } = null!;
     [Parameter] public EventCallback OnEditCompleted { get; set; }
     [Inject] protected Navigator navigator { get; set; } = null!;
-    [Inject] protected Notificator? Notificator { get; set; }
+    [Inject] protected Notificator Notificator { get; set; } = null!;
     private bool isOverdue { get; set; }
-    private DateOnly dueDate { get; set; }
-
-    private readonly CreateTariffDto _tariff = new();
+    private DateOnly dueDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    private CreateTariffDto _tariff = new() { educationalLevel = 3 };
     private readonly List<DropDownItem>? _tariffTypeItemList =
     [
         new() { Id = 2, Name = "Matrícula" },
         new() { Id = 1, Name = "Mensualidad" }
     ];
-    private readonly List<DropDownItem> _modalityItemList =
-    [
-        new() { Id = 1, Name = "Preescolar" },
-        new() { Id = 2, Name = "Primaria" },
-        new() { Id = 3, Name = "Secundaria" }
-    ];
-    private async Task CreateTariffData()
+    
+    protected override Task OnParametersSetAsync()
     {
+        isOverdue = _tariff.dueDate == null;
+        return Task.CompletedTask;
+    }
+
+    private async Task CreateTariffData()
+    
+    { 
+        if (!isOverdue && dueDate < DateOnly.FromDateTime(DateTime.Now))
+        {
+            await Notificator.ShowError("La fecha de vencimiento no puede ser anterior a la fecha actual.");
+            return;
+        }
+        
         _tariff.dueDate = isOverdue ? null : dueDate.MapToDto();
         var response = await controller.CreateTariffData(_tariff);
         if (response != null)
         {
-            await Notificator!.ShowSuccess("Se ha registrado el arancel correctamente.");
+            await Notificator.ShowSuccess("Se ha registrado el arancel correctamente.");
             await navigator.HideModal("ModalNewTariff");
             await OnEditCompleted.InvokeAsync();
+            _tariff = new() { educationalLevel = 3 };
         }
         else
         {
-            await Notificator!.ShowError( "Hubo un fallo al registrar el arancel.");
+            await Notificator.ShowError( "Hubo un fallo al registrar el arancel.");
         }
     }
 }
--- src/System/View/Secretary/Schoolyear/TariffsView/NewTariff/UpdateTariffDataModal.razor.cs
+++ src/System/View/Secretary/Schoolyear/TariffsView/NewTariff/UpdateTariffDataModal.razor.cs
@@ -19,17 +19,14 @@ public partial class UpdateTariffDataModal : ComponentBase
         new() { Id = 2, Name = "Matrícula" },
         new() { Id = 1, Name = "Mensualidad" }
     ];
-    private readonly List<DropDownItem> _modalityItemList =
-    [
-        new() { Id = 1, Name = "Preescolar" },
-        new() { Id = 2, Name = "Primaria" },
-        new() { Id = 3, Name = "Secundaria" }
-    ];
-
+    protected override Task OnParametersSetAsync()
+    {
+        isOverdue = tariffData.dueDate == null;
+        return Task.CompletedTask;
+    }
+    
     private async Task CompleteEdit()
     {
-        tariffData.dueDate = isOverdue ? null : dueDate.MapToDto();
-        
         if (!isOverdue && dueDate < DateOnly.FromDateTime(DateTime.Now))
         {
             await Notificator.ShowError("La fecha de vencimiento no puede ser anterior a la fecha actual.");
@@ -43,6 +40,7 @@ public partial class UpdateTariffDataModal : ComponentBase
             await Notificator.ShowSuccess("Se ha actualizado el arancel correctamente.");
             await navigator.HideModal("ModalEditTariff");
             await OnEditCompleted.InvokeAsync();
+            tariffData = new();
         }
         else
         {
