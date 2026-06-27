18d06d8f1f519da693803f3f3734b9b9aee95ad0
ezequiel <ezequielurbina100@gmail.com>
2026-06-27 15:37:46 -0600

    Feat: Add MonthToViewGrade.razor

--- src/Controller/PrintDocumentController.cs
+++ src/Controller/PrintDocumentController.cs
@@ -1,4 +1,5 @@
 using wsmcbl.src.Controller.Service;
+using wsmcbl.src.View.Accounting.MonthToViewGrade;
 
 namespace wsmcbl.src.Controller;
 
@@ -13,4 +14,17 @@ public class PrintDocumentController : BaseController
         return await apiFactory
             .WithNotificator.GetByteFileAsync(Modules.Secretary, "degrees/export");
     }
+
+    public async Task<List<MediaDto>> GetAllMedia()
+    {
+        var Default = new List<MediaDto>();
+        return await apiFactory.WithNotificator.GetAsync(Modules.Resources, "medias", Default);
+    }
+    
+    public async Task<bool> UpdateMedia(int month)
+    {
+        var Default = new List<MediaDto>();
+        return await apiFactory.WithNotificator.PutAsync(Modules.Resources, $"media/report-card-month?month={month}", Default);
+    }
+    
 }
